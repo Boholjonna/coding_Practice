37e664be71eb010a51a894db32a6d0237aee4dcb
Jonna Bohol BSCPE 1-G DAY PROGRAM <159886481+Boholjonna@users.noreply.github.com>
2025-04-25 15:58:51 +0800

    parcticed

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 
 
 
-    function changeText(elementId, newText, resetText, buttonResetText) {
-      const element = document.getElementById(elementId);
-      const button = event.target; // Refers to the button that was clicked
-
-      if (element.innerHTML === newText) {
-        element.innerHTML = resetText;
-        button.innerHTML = buttonResetText;
-      } else {
-        element.innerHTML = newText;
-        button.innerHTML = "Reset";
-      }
-    }
 
 
-    
- 
-   
-    
-    
-    
+// This function is called when the button is clicked
+// It changes the text of the button and the text of an element may it be p, h1, h2, etc. as long as it is text
+
+function changeText(button, buttonOldText, newButtonText, elementId, newText) {
+  const target = document.getElementById(elementId);
+
+  // Store original text in a custom attribute if not already saved
+  if (!target.hasAttribute('data-original-text')) {
+    target.setAttribute('data-original-text', target.innerText);
+  }
+
+  const originalText = target.getAttribute('data-original-text');
+
+  if (button.innerText === buttonOldText) {
+    button.innerText = newButtonText;
+    target.innerText = newText;
+  } else {
+    button.innerText = buttonOldText;
+    target.innerText = originalText;
+  }
+}
